a3e7f1a3178c23d602e14df86f80cbe4528ae9b0
Talenttris <t00316016@gmail.com>
2025-03-22 08:43:07 +0200

    Update app.py

--- app.py
+++ app.py
@@ -196,3 +196,6 @@ def gpt_interaction():
 # Run the app
 if __name__ == '__main__':
     app.run(debug=True)
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
